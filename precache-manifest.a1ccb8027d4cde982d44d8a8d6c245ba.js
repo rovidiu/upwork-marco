c91cee088572132f3160bf4dfcc65548f6187591
Ovidiu Rugina <rovidiu@gmail.com>
2020-09-26 23:26:14 +0300

    Updates

---
+++ precache-manifest.a1ccb8027d4cde982d44d8a8d6c245ba.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d35026c1a609206073ef7746fb417b1",
+    "url": "/upwork-marco/index.html"
+  },
+  {
+    "revision": "ad4aa643583640e7afb1",
+    "url": "/upwork-marco/static/css/2.6d52cc7f.chunk.css"
+  },
+  {
+    "revision": "8bf2a1495387396f7597",
+    "url": "/upwork-marco/static/css/main.55c416ca.chunk.css"
+  },
+  {
+    "revision": "ad4aa643583640e7afb1",
+    "url": "/upwork-marco/static/js/2.f81f4c2b.chunk.js"
+  },
+  {
+    "revision": "5f88874c892e024dc39c83373c2c77ee",
+    "url": "/upwork-marco/static/js/2.f81f4c2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bf2a1495387396f7597",
+    "url": "/upwork-marco/static/js/main.654eed52.chunk.js"
+  },
+  {
+    "revision": "c9ebae1532cf51eae7c2",
+    "url": "/upwork-marco/static/js/runtime-main.d5e0a9d9.js"
+  },
+  {
+    "revision": "697eab48ddbb8f3429f07510634bf42e",
+    "url": "/upwork-marco/static/media/close.697eab48.svg"
+  }
+]);
